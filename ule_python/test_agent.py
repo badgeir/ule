e152b8e6b5b5dd3f164ff5571be5e30e3a3a41c8
Peter <pleupi123@gmail.com>
2016-11-20 23:59:54 +0100

    tcp examples

--- ule_python/test_agent.py
+++ ule_python/test_agent.py
@@ -7,20 +7,15 @@ import ule
 
 def main():
     env = ule.ULEIface()
-
-    starttime = time.clock()
-    while True:
+    
+    status = 0
+    while status==0:
         try:
             img, reward, status = env.step(0)
 
             print (img.shape)
-            print(img.dtype)
             print(reward)
             print(status)
-
-            dtime = time.clock() - starttime
-            print(dtime)
-            starttime = time.clock()
 
         except Exception as e:
             sys.exit()
