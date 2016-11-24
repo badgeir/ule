6123a75a599c145e7df05d809b18ff0f99fbf26c
Peter <pleupi123@gmail.com>
2016-11-24 01:23:34 +0100

    clean test agent

--- test_agent.py
+++ test_agent.py
@@ -1,23 +1,12 @@
 import numpy as np
-import time
-import sys
-
-from matplotlib import pyplot as plt
 import ule
 
 def main():
     env = ule.load()
     
-    # observation, reward, status = env.step(1)
-    # print(observation)
-    # print(reward)
-    # print(status)
-    starttime = time.clock()
     for i in range(500):
         observation, reward, status = env.step(np.random.randint(3))
-        endtime = time.clock()
-        print(endtime - starttime)
-        starttime = endtime
+        print(observation['BallSensor'])
     
     env.close()
 
