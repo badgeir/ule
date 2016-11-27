f1d78e653fa2d260c1cdd7044321913de16fd2f5
Peter <pleupi123@gmail.com>
2016-11-27 04:12:20 +0100

    depth camera sensor

--- unityproj/Assets/ULE/Scripts/Sensor/DepthCamera.cs
+++ unityproj/Assets/ULE/Scripts/Sensor/DepthCamera.cs
@@ -2,9 +2,13 @@
 using System.Collections;
 using System.IO;
 using System.Xml.Serialization;
+using System;
+using SimpleJSON;
 
-public class DepthCamera : VirtualCamera 
+public class DepthCamera : Sensor 
 {
+    public string mName;
+
     public RenderTexture mRenderTexture;
 	public Material mDepthMaterial;
 
@@ -19,22 +23,40 @@ public class DepthCamera : VirtualCamera
         mDimY = mRenderTexture.height;
     }
 
+    public override string name()
+    {
+        return mName;
+    }
+
 	public void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
         Graphics.Blit(source, destination, mDepthMaterial);
 	}
 
-    public override byte[] GetImageBytes()
+    public override JSONNode JsonDescription()
+    {
+        JSONClass json = new JSONClass();
+        json["name"] = mName;
+        json["type"] = "camera";
+        json["width"].AsInt = mDimX;
+        json["heigth"].AsInt = mDimY;
+        json["channels"].AsInt = 3;
+        return json;
+    }
+
+    public override string SampleJson()
     {
         GetComponent<Camera>().Render();
         RenderTexture.active = mRenderTexture;
-        Texture2D tex = new Texture2D(mDimX, mDimY, TextureFormat.ARGB32, false);
+        Texture2D tex = new Texture2D(mDimX, mDimY, TextureFormat.RGB24, false);
 
         tex.ReadPixels(new Rect(0, 0, mDimX, mDimY), 0, 0);
         tex.Apply();
         RenderTexture.active = null;
 
         byte[] bytes = tex.EncodeToPNG();
-        return bytes;
+        string image = Convert.ToBase64String(bytes);
+        string json = string.Format("{{\"name\": \"{0}\", \"value\": \"{1}\"}}", mName, image);
+        return json;
     }
 }
