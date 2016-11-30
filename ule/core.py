4151b4d09a5e43354422ad679a5c06fa6d60f4bc
Peter <pleupi123@gmail.com>
2016-11-30 21:41:14 +0100

    rename member variables

--- ule/core.py
+++ ule/core.py
@@ -10,14 +10,14 @@ import json
 
 from ule.util import jsonparser
 
-def load(name='none', connectToRunning=True, tcpport=3000):
+def load(name='none', connectToRunning=True, port=3000):
     if not connectToRunning:
         if start(name, tcpport):
             print('successfully started environment %s.'%name)
         else:
             print('could not start environment %s'%name)
             return None
-    env = Env()
+    env = Env(port=port)
     env.connect()
     return env
 
@@ -39,64 +39,57 @@ def build_executable(exe_name):
 
 class Env(object):
     def __init__(self, hostIP="127.0.0.1", port=3000):
-        self.closed = False
-        self.configured = False
+        self.__closed = False
         
-        self.reward_range = (-np.inf, np.inf)
-        self._motors = None
-        self._sensors = None
+        self.__motors = None
+        self.__sensors = None
 
-        self.hostIP = "127.0.0.1"
-        self.port = port
+        self.__hostIP = "127.0.0.1"
+        self.__port = port
     
     def connect(self):
-        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.sock.connect((self.hostIP, self.port))
+        self.__sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.__sock.connect((self.__hostIP, self.__port))
 
         # get environment info
         msg = {}
         msg["method"] = "getEnvironmentInfo"
-        self.sock.send(json.dumps(msg))
-        info = self.sock.recv(16384)
+        self.__sock.send(json.dumps(msg))
+        info = self.__sock.recv(16384)
 
-        self._sensors, self._motors = jsonparser.parseSensorsAndMotors(info)
+        self.__sensors, self.__motors = jsonparser.parseSensorsAndMotors(info)
 
     def step(self):
-        import time
-
         #send action
-        jsonmotors = jsonparser.motorsToJson(self._motors)
-        self.sock.send(jsonmotors)
+        jsonmotors = jsonparser.motorsToJson(self.__motors)
+        self.__sock.send(jsonmotors)
 
-        #receive other info from environment
-        feedback = self.sock.recv(16384)
-        reward, done, info = jsonparser.parseFeedback(feedback, self._sensors)
+        #receive sensor, reward and other info from unity environment
+        feedback = self.__sock.recv(16384)
+        reward, done, info = jsonparser.parseFeedback(feedback, self.__sensors)
         return reward, done, info
 
 
     def reset(self):
         reset = {}
         reset['method'] = 'reset'
-        self.sock.send(json.dumps(reset))
-        feedback = self.sock.recv(16384)
-        jsonparser.parseFeedback(feedback, self._sensors)
+        self.__sock.send(json.dumps(reset))
+        feedback = self.__sock.recv(16384)
+        jsonparser.parseFeedback(feedback, self.__sensors)
 
 
     def close(self):
-        self.sock.close()
-        self.closed = True
+        self.__sock.close()
+        self.__closed = True
 
     def seed(self, seed=None):
         return 0
 
-    def configure(self, *args, **kwargs):
-        return False
-
     def sensors(self):
-        return self._sensors
+        return self.__sensors
     
     def motors(self):
-        return self._motors
+        return self.__motors
 
     def __del__(self):
         self.close()
